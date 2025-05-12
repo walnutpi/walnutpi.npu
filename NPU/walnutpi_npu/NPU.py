dde628f23cddf3b16e688a5e6573507f6c9c27d8
sc-bin <3335447573@qq.com>
2025-05-12 15:14:04 +0800

    优化NPU类，增加获取模型shape的功能

--- NPU/walnutpi_npu/NPU.py
+++ NPU/walnutpi_npu/NPU.py
@@ -3,49 +3,66 @@ import os
 import time
 import numpy
 
+
+class _output_buffer:
+    def count(self):
+        """获取输出buffer数量"""
+        return _awnn_lib.get_output_buffer_count(self._context_ptr)
+
+    def get(self, index: int) -> numpy.ndarray:
+        """返回指定buffer的内容"""
+        return _awnn_lib.awnn_get_output_buffer(self._context_ptr, index)
+
+    def __init__(self, context_ptr):
+        self._context_ptr = context_ptr
+
+
+class _model_data:
+    def shape(self):
+        """获取输入buffer的shape"""
+        return _awnn_lib.get_input_shape(self._context_ptr)
+
+    def __init__(self, context_ptr, model_path):
+        self._context_ptr = context_ptr
+        self.path = model_path
+
+
 class awnn:
-    context_ptr: int
 
-    class __buffer:
-        def count(self):
-            return _awnn_lib.get_output_buffer_count(
-                self.context_ptr,
-            )
-
-        def get(self, index: int) -> numpy.ndarray:
-            return _awnn_lib.awnn_get_output_buffer(self.context_ptr, index)
-
-        def __init__(self, context_ptr):
-            self.context_ptr = context_ptr
-
+    _context_ptr = 0
     model_path: str
-    output_buffer: __buffer
+    output_buffer: _output_buffer
+    model: _model_data
 
     def __init__(self, model_path: str):
-        self.model_path = model_path
-        _awnn_lib.awnn_init()
         # 判断model_path文件是否存在
-        if not os.path.exists(self.model_path):
+        if not os.path.exists(model_path):
             raise Exception(f"{model_path} file not exists")
-        if not self.model_path.endswith(".nb"):
+        if not model_path.endswith(".nb"):
             raise Exception(f"{model_path} must be a .nb file")
-        self.context_ptr = _awnn_lib.awnn_create(self.model_path)
-        self.output_buffer = self.__buffer(self.context_ptr)
+
+        _awnn_lib.awnn_init()
+
+        self._context_ptr = _awnn_lib.awnn_create(model_path)
+        self.output_buffer = _output_buffer(self._context_ptr)
+        self.model = _model_data(self._context_ptr, model_path)
 
     def __del__(self):
-        _awnn_lib.awnn_destroy(self.context_ptr)
+        if self._context_ptr == 0:
+            return
+        _awnn_lib.awnn_destroy(self._context_ptr)
         time.sleep(0.1)
-        _awnn_lib.awnn_uninit(self.context_ptr)
+        _awnn_lib.awnn_uninit(self._context_ptr)
 
     def run(self, input_data: bytearray):
         """运行模型"""
-        _awnn_lib.awnn_set_input_buffers(self.context_ptr, input_data)
-        _awnn_lib.awnn_run(self.context_ptr)
+        _awnn_lib.awnn_set_input_buffers(self._context_ptr, input_data)
+        _awnn_lib.awnn_run(self._context_ptr)
 
     def run_async(self, input_data: bytearray):
         """后台运行模型"""
-        _awnn_lib.awnn_set_input_buffers(self.context_ptr, input_data)
-        _awnn_lib.awnn_run_async(self.context_ptr)
+        _awnn_lib.awnn_set_input_buffers(self._context_ptr, input_data)
+        _awnn_lib.awnn_run_async(self._context_ptr)
 
     def is_async_running(self) -> bool:
         """后台运行模型等待"""
@@ -53,4 +70,4 @@ class awnn:
 
     def save_tensor(self, output_path: str):
         """保存输入与输出tensor到路径"""
-        _awnn_lib.awnn_dump_io(self.context_ptr, output_path)
+        _awnn_lib.awnn_dump_io(self._context_ptr, output_path)
